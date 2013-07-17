e40063bb95807555f21816a9b2417d8172b95a71
jpcarreira <jpmavcarreira@gmail.com>
2013-07-17 19:42:13 +0100

    added HITME button close #1

---
+++ BullsEye/BullsEye/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BullsEye
+//
+//  Created by João Carreira on 7/17/13.
+//  Copyright (c) 2013 João Carreira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
